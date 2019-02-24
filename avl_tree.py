96682de49f454a0e50eb9e45c2d6640eceea345e
Egon Fiedler <efiedler93@gmail.com>
2019-02-23 21:02:34 -0800

    all the elements to be made for AVL tree included

--- avl_tree.py
+++ avl_tree.py
@@ -6,4 +6,31 @@
 ##### Objectives:
 #####  * Make the AVL data structures
 
+##### What it is?
+#####  * It is a binary tree that both of it's subtrees can't
+#####  have more than one level of difference.
 
+class Node(object):
+
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+    self.height = 1
+
+class AVL_tree(object):
+
+  def get_height():
+
+
+  def get_balance():
+
+  def insert():
+
+  def left_rotate():
+
+  def right_rotate():
+
+  def search():
+
+  def remove():
