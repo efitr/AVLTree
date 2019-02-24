50306b2a5d02de00bdc987a74070bad452f33aa0
Egon Fiedler <efiedler93@gmail.com>
2019-02-24 07:06:52 -0800

    working on insert and getting the balancing to happen

--- avl_tree.py
+++ avl_tree.py
@@ -31,9 +31,31 @@ class AVL_tree(object):
     
     return self.get_height(root.left) - self.get_height(root.right)
 
-  def insert():
+  def insert(self, root, key):
+
+    #Approach:
+    # * Given that both sides have to be at most with one having
+    #   one extra level in any side, when this breaks this has to
+    #   rebalance itself rotating to the right or left
+    # * Use get_height method to know that this rule is kept
+    # * Use get_balance to know the current circunstances of the tree
+
+    #Logic
+    # * Do a binary search tree
+    # * Update the height of the ancestor node
+    # * Get the balance factor
+    # * Shape things to rotate left or right
+
+    if not root:
+      return Node(key)
+    elif key < root.val:
+      root.left = self.insert(root.left, key)
+    else:
+      root.right = self.insert(root.right, key)
 
   def left_rotate():
+    # Approach
+    # * Get it to rotate to the right
 
   def right_rotate():
 
