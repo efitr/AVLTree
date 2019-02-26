30d4466fa28bb9bddc8a65074e6551a0c53b154a
Egon Fiedler <efiedler93@gmail.com>
2019-02-26 13:31:33 -0800

    getting further on insert, added a root node to initialize, going for rotate right and rotate left

--- avl_tree.py
+++ avl_tree.py
@@ -1,4 +1,3 @@
-
 #############################################
 ##### AVL TREES
 #############################################
@@ -10,6 +9,10 @@
 #####  * It is a binary tree that both of it's subtrees can't
 #####  have more than one level of difference.
 
+##### What separates it from a Binary Search Tree?
+#####  * The aspects that are different are insertion and removal
+#####  
+
 class Node(object):
 
   def __init__(self, val):
@@ -20,18 +23,48 @@ class Node(object):
 
 class AVL_tree(object):
 
+  def __init__(self):
+    self.root = None
+
   def get_height(self, root):
+    # Purpose
+    #   * Get the current height of the node you are at
+    # Approach
+    #   * If it is not at the root position, return 0
+    #   * Return the height of the node
+
     if not root:
       return 0
     return root.height
 
   def get_balance():
+    # Purpose
+    #   *
+    # Approach
+    #   *
+    
     if not root:
       return 0
     
     return self.get_height(root.left) - self.get_height(root.right)
+  
+  def left_rotate():
+    # Purpose 
+    #   * When the height on the right side is bigger tha
+    # Approach
+    # * Get it to rotate to the right
+    node.right
+    node.left
 
-  def insert(self, root, key):
+  def right_rotate():
+
+
+  def insert(self, key, node=None):
+    # BIG(O)Notation:
+    # Expected Time Complexity: O(log(N))
+    # Expected Space Complexity: O(log(N))
+    # Current Time Complexity: ???
+    # Current Space Complexity: ???
 
     #Approach:
     # * Given that both sides have to be at most with one having
@@ -46,19 +79,29 @@ class AVL_tree(object):
     # * Get the balance factor
     # * Shape things to rotate left or right
 
-    if not root:
+    if self.root == None:
+      self.root = Node(key)
+      return root
+
+    if not node:
       return Node(key)
-    elif key < root.val:
-      root.left = self.insert(root.left, key)
+
+    elif key < node.val:
+      node.left = self.insert(key, node.left)
     else:
-      root.right = self.insert(root.right, key)
+      node.right = self.insert(key, node.right)
 
-  def left_rotate():
-    # Approach
-    # * Get it to rotate to the right
 
-  def right_rotate():
+
+    # This is meant when the value on the right 
+    if root.left.get_height < root.right.get_height:
 
   def search():
+    # BIG(O)Notation:
+    # Expected Time Complexity: O(log(N))
+    # Expected Space Complexity: O(log(N))
 
   def remove():
+    # BIG(O)Notation:
+    # Expected Time Complexity: O(log(N))
+    # Expected Space Complexity: O(log(N))
