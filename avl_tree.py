0a6476710a5942357dd9114f6486388d922e9133
Egon Fiedler <efiedler93@gmail.com>
2019-02-26 14:50:25 -0800

    got the left rotate done with one approach, still working at the second one, the insert function is coming close with the rotations almost done

--- avl_tree.py
+++ avl_tree.py
@@ -9,8 +9,8 @@
 #####  * It is a binary tree that both of it's subtrees can't
 #####  have more than one level of difference.
 
-##### What separates it from a Binary Search Tree?
-#####  * The aspects that are different are insertion and removal
+##### What functions are different from a Binary Search Tree?
+#####  * The functions that are different are insertion and removal
 #####  
 
 class Node(object):
@@ -49,12 +49,27 @@ class AVL_tree(object):
     return self.get_height(root.left) - self.get_height(root.right)
   
   def left_rotate():
+    # Note:
+    #   * This is just moving pointers, focus on that
     # Purpose 
-    #   * When the height on the right side is bigger tha
-    # Approach
-    # * Get it to rotate to the right
-    node.right
-    node.left
+    #   * When the height from a node is two, rebalance to make it be one once more
+    # Approach 1
+    #   * Navigate from the node through the right pointer and from the sub node been
+    #     pointed at to the left pointing pointer and make that point to the node
+    #   * Make the pointer pointing to the node, now point to the sub node on the right 
+    #     
+    node.right.left = node
+    node = node.right
+
+    # Approach 2
+    #   * Make a temporary variable that is the current node
+    #   * Make the right pointer be the node
+    #   * 
+    #
+    #
+    temp = node
+    node.right = node
+    node.left = temp
 
   def right_rotate():
 
@@ -74,6 +89,7 @@ class AVL_tree(object):
     # * Use get_balance to know the current circunstances of the tree
 
     #Logic
+    # * If there is no root, make it.
     # * Do a binary search tree
     # * Update the height of the ancestor node
     # * Get the balance factor
@@ -82,19 +98,28 @@ class AVL_tree(object):
     if self.root == None:
       self.root = Node(key)
       return root
-
+    # Do I still need this line of code??
     if not node:
       return Node(key)
-
+    # Getting the node to it's corresponding position on the 
+    # binary search tree (it becomes AVL when you add the rotations)
     elif key < node.val:
       node.left = self.insert(key, node.left)
     else:
       node.right = self.insert(key, node.right)
 
+    # Check balance factor (you are using the 
+    # height of the node in the left - the one in the right )
+    # 
+    balance_factor = node.get_balance()
 
+    # For rotation to happen the balance factor must 
+    # be higher than one
+    if balance_factor == -2:
+      # Here should rotate right
+    if balance factor == 2:
+      # Here should rotate left
 
-    # This is meant when the value on the right 
-    if root.left.get_height < root.right.get_height:
 
   def search():
     # BIG(O)Notation:
