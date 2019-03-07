5b322e9f645ce6aff152c0bcc3132a7a586a340b
Egon Fiedler <efiedler93@gmail.com>
2019-03-06 22:47:04 -0800

    improving the initializer

--- avl_tree_test.py
+++ avl_tree_test.py
@@ -1,12 +1,29 @@
 #!python
 
-from binaryheap import BinaryMinHeap
+from avl_tree import AVLTree
 import random
 import unittest
 
 
 class TestAVLTree(unittest.TestCase):
-   
+    def test_insert(self):
+        avlTree = AVLTree()
+        numbers = [10, 5, 1]
+
+    def test_rotations(self):
+
+    def test_rotations(self):
+
+    def test_rotations(self):
+
+    def test_rotations(self):
+
+    def test_balance_factor(self):
+
+    def test_contains(self):
+    
+    def test_search(self):
+
 
 if __name__ == '__main__':
     unittest.main()
