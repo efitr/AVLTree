58ed5e3e79a549ab22b22ed52c0018df88aec2a0
Egon Fiedler <efiedler93@gmail.com>
2019-03-08 06:56:06 -0800

    variable refactoring

--- avl_tree.py
+++ avl_tree.py
@@ -26,66 +26,60 @@ class Node(object):
     self.right = None
     self.height = 1
 
+  def _get_height(self):
+    """Return the height of this tree (the number of edges on the longest
+    downward path from this tree's root node to a descendant leaf node).
+    TODO: Best and worst case running time: ??? under what conditions?"""
+    # TODO: Check if root node has a value and if so calculate its height
+
+    # Check if left child has a value and if so calculate its height
+    height_left_pointer_until_leaf = 0
+    if self.left is not None:
+        height_left_pointer_until_leaf = self.left.height() + 1
+    
+    # Check if right child has a value and if so calculate its height
+    height_right_pointer_until_leaf = 0
+    if self.right is not None:
+        height_right_pointer_until_leaf = self.right.height() + 1
+
+    return height_left_pointer_until_leaf, height_right_pointer_until_leaf
+
 class AVLTree(object):
 
   def __init__(self, numbers=None):
-    # This is handled at the first call of the function
-    # insert(value, current_node=None)
+
     self.root = None
-    self.size = 0
 
     if numbers is not None:
       for number in numbers:
-          self.insert_left_or_right_measure_height_rotate_maybe(number)
+          self.insert_left_or_right_measure_rule_rotate_maybe(number)
 
-  # Helper functions
-  def _get_height(self, root):
-    # Purpose
-    #   * Get the current height of the node you are at
-    # Approach
-    #   * If it is not at the root position, return 0
-    #   * Return the height of the node
-    # How is this actually working??
-    # if not root:
-    #   return 0
-    return root.height
+  def _measure_balance_rule(self, node):
 
-  def _measure_height(self, left_node, right_node):
-    # In the what part of the process this interacts that actually
-
-    # if not root:
-    #   return 0
-    return self._get_height(left_node) - self._get_height(right_node)
+    left_node_height, right_node_height = node._get_height()
+    return left_node_height - right_node_height
   
   # Rotation Helper Functions
   def _left_rotate(self, node_right, node_left=None): #Maybe add the right and left node
-    # Note:
-    #   * This is just moving pointers, focus on that
-    # Purpose 
-    #   * When the height from a node is two, rebalance to make it be one once more
-    # Approach 1
-    #   * Navigate from the node through the right pointer and from the sub node been
-    #     pointed at to the left pointing pointer and make that point to the node
-    #   * Make the pointer pointing to the node, now point to the sub node on the right 
-    #     
-    #### If statement that depending on what conditions do what it's suppose to do
-    #### there happens and double rotation
+
     if node_left is not None:
       #Whatever is needed for the extra rotation
-      continue
-    node.right.left = node
-    return node.right
+      node.left = node.left.right
+      node.left.right.left = node.left
 
-
+    node.left = node
+    node = node.left.right
 
   def _right_rotate(self, node_left, node_right=None):
     if node_right is not None:
-      continue
-    node.right.right = node
-    node = node.left
+      node.right.left = node.right
+      node.right = node.right.left.right
+
+    node.right.left = node
+    node = node.right
 
   # Main Functions
-  def insert_left_or_right_measure_height_rotate_maybe(self, number, current_node=None):
+  def insert_left_or_right_measure_rule_rotate_maybe(self, number, current_node=None):
     # BIG(O)Notation:
     # Expected Time Complexity: O(log(N))
     # Expected Space Complexity: O(log(N))
@@ -121,54 +115,15 @@ class AVLTree(object):
     # Check balance factor (you are using the 
     # height of the node in the left - the one in the right )
     # 
-    current_height = node._measure_height()
+    current_height = self._measure_balance_rule(node)
 
     # For rotation to happen the balance factor must 
     # be higher than one
     # There are four possible scenarios for the rotation
     if current_height == -2:
       # Here should rotate right
-      node.right_rotate()
+      node._left_rotate()
     elif current_height == 2:
       # Here should rotate left
-      node.left_rotate()
+      node._right_rotate()
 
-  def search():
-    """Insert the given item in order into this binary search tree.
-    TODO: Best case running time: ??? under what conditions?
-    TODO: Worst case running time: ??? under what conditions?"""
-    # Handle the case where the tree is empty
-    if self.is_empty():
-      # TODO: Create a new root node
-      self.root = Node(item)
-      # TODO: Increase the tree size
-      self.size += 1
-      return
-    # Find the parent node of where the given item should be inserted
-    parent = self._find_parent_node_recursive(item, self.root)
-    # TODO: Check if the given item should be inserted left of parent node
-    if ...:
-      # TODO: Create a new node and set the parent's left child
-      parent.left = ...
-    # TODO: Check if the given item should be inserted right of parent node
-    elif ...:
-      # TODO: Create a new node and set the parent's right child
-      parent.right = ...
-    # TODO: Increase the tree size
-    self.size +=1
-
-  def deletion():
-    # BIG(O)Notation:
-    # Expected Time Complexity: O(log(N))
-    # Expected Space Complexity: O(log(N))
-    # Current Time Complexity: ???
-    # Current Space Complexity: ???
-    pass
-
-  def find_smallest_element():
-    # Just move along the node.left property recursively until the node.left is none, return that node value
-    pass
-  
-  def find_largest_element():
-    # Just move along the node.right property recursively until the node.right is none, return that node value
-    pass
