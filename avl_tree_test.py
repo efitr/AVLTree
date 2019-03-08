1af8ec56ca360ce07177d9a471bff9b9946b7979
Egon Fiedler <efiedler93@gmail.com>
2019-03-08 07:15:48 -0800

    made the first test

--- avl_tree_test.py
+++ avl_tree_test.py
@@ -1,93 +1,69 @@
 #!python
 
-from avl_tree import AVLTree
+from avl_tree import AVLTree, Node
 import random
 import unittest
 
-class BinaryTreeNodeTest(unittest.TestCase):
+class AVLNodeTest(unittest.TestCase):
 
     def test_init(self):
         data = 123
-        node = BinaryTreeNode(data)
-        assert node.data is data
+        node = Node(number)
+        assert node.number is number
         assert node.left is None
         assert node.right is None
-
-    def test_is_leaf(self):
-        # Create node with no children
-        node = BinaryTreeNode(2)
-        assert node.is_leaf() is True
-        # Attach left child node
-        node.left = BinaryTreeNode(1)
-        assert node.is_leaf() is False
-        # Attach right child node
-        node.right = BinaryTreeNode(3)
-        assert node.is_leaf() is False
-        # Detach left child node
-        node.left = None
-        assert node.is_leaf() is False
-        # Detach right child node
-        node.right = None
-        assert node.is_leaf() is True
-
-    def test_is_branch(self):
-        # Create node with no children
-        node = BinaryTreeNode(2)
-        assert node.is_branch() is False
-        # Attach left child node
-        node.left = BinaryTreeNode(1)
-        assert node.is_branch() is True
-        # Attach right child node
-        node.right = BinaryTreeNode(3)
-        assert node.is_branch() is True
-        # Detach left child node
-        node.left = None
-        assert node.is_branch() is True
-        # Detach right child node
-        node.right = None
-        assert node.is_branch() is False
+        assert node.height = 1
 
     def test_height(self):
         # Create node with no children
-        node = BinaryTreeNode(4)
+        node = Node(4)
         assert node.height() == 0
         # Attach left child node
-        node.left = BinaryTreeNode(2)
+        node.left = Node(2)
         assert node.height() == 1
         # Attach right child node
-        node.right = BinaryTreeNode(6)
+        node.right = Node(6)
         assert node.height() == 1
         # Attach left-left grandchild node
-        node.left.left = BinaryTreeNode(1)
+        node.left.left = Node(1)
         assert node.height() == 2
         # Attach right-right grandchild node
-        node.right.right = BinaryTreeNode(8)
+        node.right.right = Node(8)
         assert node.height() == 2
         # Attach right-right-left great-grandchild node
-        node.right.right.left = BinaryTreeNode(7)
+        node.right.right.left = Node(7)
         assert node.height() == 3
 
 
 class TestAVLTree(unittest.TestCase):
 
-    def test_insert_left_or_right_measure_height_rotate_maybe(self):
-        numbers = [10, 5, 1]
+    def test_insert_left_or_right_measure_rule_rotate_maybe(self):
+        numbers = [1, 2, 3]
         avlTree = AVLTree(numbers)
+        assert avlTree.root.number == 1
+        assert avlTree.root.left.number == 2
+        assert avlTree.root.left.number == 3
+
+        assert avlTree.root.height = 1
+        assert avlTree.root.left.height = 2
+        assert avlTree.root.right.height = 2
+
+
         
 
-    def test_rotations(self):
+#     def test_rotations(self):
 
-    def test_rotations(self):
+#     def test_rotations(self):
 
-    def test_rotations(self):
+#     def test_rotations(self):
 
-    def test_rotations(self):
+#     def test_rotations(self):
 
-    def test_balance_factor(self):
+#     def test_balance_factor(self):
 
-    def test_contains(self):
+#     def test_contains(self):
     
-    def test_search(self):
+#     def test_search(self):
 
 
 if __name__ == '__main__':
